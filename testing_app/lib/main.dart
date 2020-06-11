67e83331368765d463d987c15bd2a74434934b96
robthomas <robthomas@expedia.com>
2020-06-11 18:02:19 +0530

    Hello World Example

---
+++ testing_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
